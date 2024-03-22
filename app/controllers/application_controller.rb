e2ecf10305326754c21dae6da0ad195a73fa348c
FLAME <xaustin03@gmail.com>
2024-03-22 10:50:02 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     before_action :set_csrf_cookie
 
     def set_csrf_cookie
-        cookies['XSRF-TOKEN'] = { value:  form_authenticity_token, secure: Rails.env.development?, domain: %W(#{request.host}) }
+        cookies['XSRF-TOKEN'] = { value:  form_authenticity_token, secure: Rails.env.development?, domain: %W(#{request.host}, .onrender.com) }
     end
 end
