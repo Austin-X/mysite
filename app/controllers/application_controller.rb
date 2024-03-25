cf9f0505c9d2d903ee3499e1c0ecab74bf994dbd
FLAME <xaustin03@gmail.com>
2024-03-25 18:40:50 -0400

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
     before_action :set_csrf_cookie
 
     def set_csrf_cookie
+        $hst = request.host
+        $dom = request.domain
         cookies['XSRF-TOKEN'] = { value:  form_authenticity_token, secure: Rails.env.development?, domain: ['lawl', 'testing'] }
     end
 end
