ab81661e99863c23db6536c945192b3489d99261
FLAME <xaustin03@gmail.com>
2024-03-31 22:32:13 -0400

    Add scoping to books

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -5,6 +5,7 @@ module Types
     field :id, ID, null: false
     field :name, String
     field :published, Integer
+    field :books, [Types::BookType]
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -2,6 +2,15 @@
 
 module Types
   class BookType < Types::BaseObject
+    def self.authorized?(object, context)
+      object.id < 10
+    end
+
+    def self.scope_items(items, context)
+      items.each { |x| puts "ID: #{x.id}" }
+      items.where('id < 10')
+    end
+
     field :id, ID, null: false
     field :published_at, GraphQL::Types::ISO8601DateTime, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
