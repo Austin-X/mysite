f3931c8a9071fa067b8bb490ab91b0559a6f2f4d
FLAME <xaustin03@gmail.com>
2024-05-06 15:43:05 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,9 @@ class ApplicationController < ActionController::Base
     $hst = request.host
     $dom = request.domain
 
+    cookies.delete('XSRF-TOKEN', domain: '.mailtrap123.club')
+
     cookies['XSRF-TOKEN'] =
-      { value: form_authenticity_token, secure: !Rails.env.development?, domain: ['.mailtrap123.club'] }
+      { value: form_authenticity_token, secure: !Rails.env.development? }
   end
 end
